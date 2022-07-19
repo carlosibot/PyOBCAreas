fa2213c732101f6a997558f8ff010afee2654a12
carlosibot <carlosibot@gmail.com>
2022-07-18 21:46:37 -0400

    Agregando proyecto

---
+++ Areas.py
@@ -0,0 +1,12 @@
+import math
+def areaTriangulo(b,a):
+    Area = (b * a) / 2
+    return Area
+print("El valor del area del triangulo es: ", areaTriangulo(3,5))
+def areaCirculo(r):
+    Areat = math.pi * r**2
+    return Areat
+print("El valor del area del circulo es: ",("{:.2f}".format (areaCirculo(5))))
+
+
+
